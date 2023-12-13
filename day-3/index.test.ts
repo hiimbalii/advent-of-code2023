83aa2ac537bef570965b09b49636485fd0aa1d76
hiimbalii <tcrbt1@gmail.com>
2023-12-13 01:45:24 +0100

    wip

--- day-3/index.test.ts
+++ day-3/index.test.ts
@@ -1,9 +1,47 @@
 import * as fs from "fs";
-import { run } from "./index";
+import { Index, Matrix, findSymbols, run } from "./index";
 
 const lines = fs.readFileSync("./input").toString().split("\n");
 const lines_valid = fs.readFileSync("./input.validate").toString().split("\n");
 
+describe("findSymbols", () => {
+  const input: Matrix = [
+    [".", ".", "3", "*", "."],
+    ["4", "-", "$", "+", "."],
+  ];
+  it("should not find numbers", () => {
+    const numbers: Index[] = [
+      [0, 2],
+      [1, 0],
+    ];
+    const [result] = findSymbols(input);
+    expect(result).not.toEqual(expect.arrayContaining(numbers));
+  });
+  it("should not find dots", () => {
+    const dots: Index[] = [
+      [0, 0],
+      [0, 1],
+      [0, 4],
+      [1, 4],
+    ];
+    const [result] = findSymbols(input);
+    expect(result).not.toEqual(expect.arrayContaining(dots));
+  });
+  it("should find everything else", () => {
+    const symbols: Index[] = [
+      [0, 2],
+      [1, 1],
+      [1, 2],
+      [1, 3],
+    ];
+    const [result] = findSymbols(input);
+    expect(result).toEqual(expect.arrayContaining(symbols));
+  });
+  it("should return the matrix", () => {
+    const [_, matrix] = findSymbols(input);
+    expect(matrix).toEqual(input);
+  });
+});
 
 it("validate", () => {
   const result = run(lines_valid);
--- day-3/index.ts
+++ day-3/index.ts
@@ -1,14 +1,43 @@
-//
-// take all the numbers and their x,ys
-// take the sybolds and their x,ys
-// find all numbers with a symbold neraby?
 export const run = (input: string[]) => {
-  const part_number: number[] = input
+  const characters: Matrix = input
     .filter((str) => str.length)
-    .map(solve);
-  console.log(largest_colors)
-  return largest_colors.reduce((prev, curr) => prev + curr , 0);
+    .map((str) => str.split(""));
+
+  return findNumbers(characters).reduce((prev, curr) => prev + curr, 0);
 };
 
-export const solve = (line: string) => 
-//
+export type Matrix = string[][];
+export type Index = [number, number];
+// I am aware it's indices, but this is way more readable
+type IndexesWithMatrix = [Index[], Matrix];
+
+const isNumber = (char: string) => !isNaN(parseInt(char));
+const isSymbol = (char: string) => {
+  return ["+", "-"].includes(char) && !isNumber(char) && char !== ".";
+};
+// Not happy with the name but it's late lol
+const symbolToIndex = (row: string[], outerIndex: number): Index[] =>
+  row
+    .map((char, innerIndex): [string, number, number] => [
+      char,
+      outerIndex,
+      innerIndex,
+    ])
+    .filter(([char]) => isSymbol(char))
+    .map(([_, ...indexes]) => indexes);
+export const findSymbols = (input: Matrix): IndexesWithMatrix => [
+  input.flatMap(symbolToIndex),
+  input,
+];
+
+export const findDigits = ([
+  indexes,
+  board,
+]: IndexesWithMatrix): IndexesWithMatrix => [indexes, board];
+
+export const walkNumbers = ([indexes, board]: IndexesWithMatrix): number[] => [
+  0,
+];
+
+export const findNumbers = (input: Matrix): number[] =>
+  walkNumbers(findDigits(findSymbols(input)));
