0da9678dc3440084490af0cc6894141c22c1274f
Daniel Toth <daniel.toth@adidas.com>
2023-12-18 16:42:18 +0100

    finish d-7

--- day-7/src/lib.rs
+++ day-7/src/lib.rs
@@ -99,34 +99,24 @@ impl From<&str> for Hand {
     fn from(value: &str) -> Self {
         let cards: Vec<Card> = value.chars().map(Card::from).collect();
         let mut cards_map: HashMap<Card, usize> = HashMap::new();
-        for card in cards {
+        for card in &cards {
             if cards_map.contains_key(&card) {
                 let card = cards_map.get_mut(&card).unwrap();
                 *card += 1;
             } else {
-                cards_map.insert(card, 1);
+                cards_map.insert(card.clone(), 1);
             }
         }
         let mut cards_map: Vec<(Card, usize)> = cards_map.into_iter().collect();
         cards_map.sort_by(|a, b| b.1.cmp(&a.1));
         return match (cards_map.len(), cards_map.first().unwrap()) {
-            (1, (card, 5)) => Hand::FiveOfAKind(cards),
-            (2, (card, 3)) => Hand::FullHouse(cards),
-            (2, (card, 4)) => Hand::FourOfAKind(cards),
-            (3, (card, 3)) => {
-                if cards_map[2].0 > cards_map[1].0 {
-                    return Hand::ThreeOfAKind(cards);
-                }
-                Hand::ThreeOfAKind(cards)
-            }
-            (3, (card, 2)) => {
-                if card.to_owned() > cards_map[1].0 {
-                    return Hand::TwoPairs(cards);
-                }
-                Hand::TwoPairs(cards)
-            }
-            (4, (card, 2)) => Hand::OnePair(cards),
-            (5, _) => Hand::HighHand(cards),
+            (1, (_, 5)) => Hand::FiveOfAKind(cards),
+            (2, (_, 3)) => Hand::FullHouse(cards),
+            (2, (_, 4)) => Hand::FourOfAKind(cards),
+            (3, (_, 3)) => Hand::ThreeOfAKind(cards),
+            (3, (_, 2)) => Hand::TwoPairs(cards),
+            (4, (_, 2)) => Hand::OnePair(cards),
+            (5, (_, 1)) => Hand::HighHand(cards),
             _ => panic!(),
         };
     }
@@ -149,7 +139,12 @@ impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         let result = usize::from(self).cmp(&usize::from(other));
         if let Ordering::Equal = result {
-            return self.get_hand().into_iter().enumerate().map(|x|x.cmp(other));
+            return self
+                .get_hand()
+                .into_iter()
+                .enumerate()
+                .map(|(idx, x)| x.cmp(&other.get_hand()[idx]))
+                .find(|x| x.ne(&Ordering::Equal));
         } else {
             return Some(result);
         };
@@ -179,7 +174,6 @@ pub fn solve<'a>(input: impl Iterator<Item = &'a str>) -> usize {
         .enumerate()
         .map(|(rank, line)| (rank + 1) * line.1)
         .collect();
-    dbg!(&lines);
     lines.into_iter().sum()
 }
 
@@ -199,25 +193,70 @@ mod test {
 
     #[test]
     fn parse_hands() {
-        assert_eq!(Hand::from("23456"), Hand::HighHand);
+        assert_eq!(
+            Hand::from("23456"),
+            Hand::HighHand(vec![
+                Card::Two,
+                Card::Three,
+                Card::Four,
+                Card::Five,
+                Card::Six
+            ])
+        );
         //Five of a kind, where all five cards have the same label:
-        assert_eq!(Hand::from("AAAAA"), Hand::FiveOfAKind(Card::Ace));
+        assert_eq!(
+            Hand::from("AAAAA"),
+            Hand::FiveOfAKind(vec![Card::Ace, Card::Ace, Card::Ace, Card::Ace, Card::Ace])
+        );
         //Four of a kind, where four cards have the same label and one card has a different label:
         assert_eq!(
             Hand::from("AA8AA"),
-            Hand::FourOfAKind(Card::Ace, Card::Eight)
+            Hand::FourOfAKind(vec![
+                Card::Ace,
+                Card::Ace,
+                Card::Eight,
+                Card::Ace,
+                Card::Ace
+            ])
         );
         //Full house, where three cards have the same label, and the remaining two cards share a different label:
-        assert_eq!(Hand::from("32323"), Hand::FullHouse(Card::Three, Card::Two));
+        assert_eq!(
+            Hand::from("32323"),
+            Hand::FullHouse(vec![
+                Card::Three,
+                Card::Two,
+                Card::Three,
+                Card::Two,
+                Card::Three
+            ])
+        );
         //Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand:
         assert_eq!(
             Hand::from("T9T8T"),
-            Hand::ThreeOfAKind(Card::T, Card::Nine, Card::Eight)
+            Hand::ThreeOfAKind(vec![Card::T, Card::Nine, Card::T, Card::Eight, Card::T])
         );
         //Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label:
-        assert_eq!(Hand::from("33242"), Hand::TwoPairs(Card::Three, Card::Two));
+        assert_eq!(
+            Hand::from("33242"),
+            Hand::TwoPairs(vec![
+                Card::Three,
+                Card::Three,
+                Card::Two,
+                Card::Four,
+                Card::Two
+            ])
+        );
         //One pair, where two cards share one label, and the other three cards have a different label from the pair and each other:
-        assert_eq!(Hand::from("A234A"), Hand::OnePair(Card::Ace));
+        assert_eq!(
+            Hand::from("A234A"),
+            Hand::OnePair(vec![
+                Card::Ace,
+                Card::Two,
+                Card::Three,
+                Card::Four,
+                Card::Ace
+            ])
+        );
 
         //High card, where all cards' labels are distinct:
     }
